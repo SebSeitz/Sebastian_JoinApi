a1393fb4f5476fc2ed5f90685d7c179e3ec08071
SebSeitz <sebastian_seitz13@web.de>
2022-11-07 22:37:13 +0100

    added first models

--- Kanban/models.py
+++ Kanban/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Category(models.Model):
+    management = models.CharField(max_length=30)
+    sales = models.CharField(max_length=30)
+    marketing = models.CharField(max_length=30)
+    product = models.CharField(max_length=30)
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    due_date = models.DateField(default=datetime.date.today)
+    category = models.ForeignKey(Category)
+
+
