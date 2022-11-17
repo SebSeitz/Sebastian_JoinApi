8deae1686d03f2e9217e1a93c17ce35def92252f
SebSeitz <sebastian_seitz13@web.de>
2022-11-17 10:01:17 +0100

    update categorie

--- Kanban/admin.py
+++ Kanban/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Category, Tasks
 
-admin.site.register(Category, Tasks)
+admin.site.register(Category)
+admin.site.register(Tasks)
--- Kanban/models.py
+++ Kanban/models.py
@@ -2,11 +2,23 @@ from django.db import models
 import datetime
 from django.conf import settings
 
+
+
 class Category(models.Model):
-    management = models.CharField(max_length=30)
-    sales = models.CharField(max_length=30)
-    marketing = models.CharField(max_length=30)
-    product = models.CharField(max_length=30)
+    CATEGORY_CHOICES = [
+        ('MT','MANAGEMENT'),
+        ('SL', 'SALES'),
+        ('MK','MARKETING'),
+        ('PR','PRODUCT'),
+    ]
+    category_id = models.AutoField(primary_key=True, default=0)
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default='MT')
+
+# class Category(models.Model):
+#     management = models.CharField(max_length=30)
+#     sales = models.CharField(max_length=30)
+#     marketing = models.CharField(max_length=30)
+#     product = models.CharField(max_length=30)
 
 class Tasks(models.Model):
     title = models.CharField(max_length=30)
