f494949fc137946014044c11c3bceb4424c9ae8c
SebSeitz <sebastian_seitz13@web.de>
2022-11-17 11:06:01 +0100

    removed category class

--- Kanban/models.py
+++ Kanban/models.py
@@ -5,21 +5,23 @@ from django.contrib.auth.models import User
 
 
 
-class Category(models.Model):
-    CATEGORY_CHOICES = [
-        ('MT','MANAGEMENT'),
-        ('SL', 'SALES'),
-        ('MK','MARKETING'),
-        ('PR','PRODUCT'),
-    ]
-    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default='MT')
+# class Category(models.Model):
+
+#     category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default='MT')
 
 class Task(models.Model):
-    title = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
-    due_date = models.DateField(default=datetime.date.today)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    user = models.ForeignKey(
+
+  CATEGORY_CHOICES = [
+  ('MT','MANAGEMENT'),
+  ('SL', 'SALES'),
+  ('MK','MARKETING'),
+  ('PR','PRODUCT'),
+]
+  title = models.CharField(max_length=30)
+  description = models.CharField(max_length=30)
+  due_date = models.DateField(default=datetime.date.today)
+  category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default='MT')
+  user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
     )
