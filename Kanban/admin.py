3ec83e642a314198895745bc85df7daf212ffc86
SebSeitz <sebastian_seitz13@web.de>
2022-11-17 10:55:42 +0100

    deleted and added sql again

--- Kanban/admin.py
+++ Kanban/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Tasks
+from .models import Category, Task
 
 admin.site.register(Category)
-admin.site.register(Tasks)
+admin.site.register(Task)
--- Kanban/models.py
+++ Kanban/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 
@@ -11,22 +12,15 @@ class Category(models.Model):
         ('MK','MARKETING'),
         ('PR','PRODUCT'),
     ]
-    category_id = models.AutoField(primary_key=True, default=0)
     category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default='MT')
 
-# class Category(models.Model):
-#     management = models.CharField(max_length=30)
-#     sales = models.CharField(max_length=30)
-#     marketing = models.CharField(max_length=30)
-#     product = models.CharField(max_length=30)
-
-class Tasks(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
     due_date = models.DateField(default=datetime.date.today)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE,
     )
 
