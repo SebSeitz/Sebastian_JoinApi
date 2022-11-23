c071452e66dc459fa09ab3b67e0df40f033dc7c7
SebSeitz <sebastian_seitz13@web.de>
2022-11-23 21:56:53 +0100

    latest commit

--- Kanban/serializers.py
+++ Kanban/serializers.py
@@ -2,8 +2,14 @@ from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Task
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+      class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'due_date', 'category']
+        fields = ['id', 'title', 'description', 'due_date', 'category', 'user']
--- up.bat
+++ up.bat
@@ -1,3 +1,4 @@
 git add .
 git commit -m "%*"
 git push
+start chrome "https://www.pythonanywhere.com/user/SebastianSeitz36/consoles/26165160/"
