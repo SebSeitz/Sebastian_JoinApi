768dc78f9775285e6df3590a90610c3a5cce3209
SebSeitz <sebastian_seitz13@web.de>
2022-12-30 21:22:15 +0100

    last commit of 22

--- Kanban/models.py
+++ Kanban/models.py
@@ -27,3 +27,12 @@ class Task(models.Model):
     )
 
 
+  class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=80)
+
+
+
+
