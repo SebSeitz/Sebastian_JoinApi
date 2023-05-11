a83751824b3fb5765dc6da20a5c148894147abcc
SebSeitz <sebastian_seitz13@web.de>
2023-05-11 10:48:53 +0200

    registered Subtask under admin

--- Kanban/admin.py
+++ Kanban/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Task, MyUser
+from .models import Task, MyUser, Subtask
 
 admin.site.register(Task)
 admin.site.register(MyUser)
+admin.site.register(Subtask)
